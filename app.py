b993e5fdda8edd41be5b87db81f3b0b5bfd5358d
miles hill <miles1618@gmail.com>
2017-02-10 14:14:16 -0600

    Added index file and templates

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import requests
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -9,9 +11,19 @@ db = SQLAlchemy(app)
 
 from models import Result
 
-@app.route('/')
-def hello():
-    return "hello world"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == 'POST':
+        # get url that user entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append('Unable to get URL')
+    return render_template('index.html', errors=errors, results=results)
 
 @app.route('/<name>')
 def hello_name(name):
